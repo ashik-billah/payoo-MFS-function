3d3aff6eca8596445aa7eb4841a7d6256f1513bd
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-02-28 23:38:54 +0600

    add money using share function

--- js/addMoney.js
+++ js/addMoney.js
@@ -10,7 +10,15 @@
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     
     event.preventDefault();
-    
+
+    console.log('add money button cliked');
+//    const addMoney = getInputFieldValueById();
+//    console.log(addMoney);
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+
+    console.log('add money with parameter',addMoney);
+    console.log('add pin number with parameter',pinNumber);
 
 
-})
+});
--- js/utilites.js
+++ js/utilites.js
@@ -1,1 +1,13 @@
-console.log('utilites added')
+// console.log('utilites added')
+// common share function here
+
+// function getInputFieldValueById(){
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+function getInputFieldValueById(id){
+ const inputValue = document.getElementById(id).value;
+ return inputValue;
+}
