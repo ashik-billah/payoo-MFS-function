b5ed52f97fd30247609aca09fccec7c779066e76
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-03-01 23:50:16 +0600

    add toggle features

---
+++ js/features.js
@@ -0,0 +1,6 @@
+document.getElementById('show-add-money-form').addEventListener('click', function(){
+
+
+    console.log('show add money button');
+    showSectionById('add-money-form');
+})
--- js/utilites.js
+++ js/utilites.js
@@ -29,3 +29,6 @@ function getTextFieldValueById(id){
 
 }
 
+function showSectionById(id){
+    
+}
